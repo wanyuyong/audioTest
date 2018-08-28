c965b4df9e3198e7c844595244639c8ce38166ed
ChaoYi <chaoyi@wacai.com>
2018-08-28 23:38:41 +0800

    no message

--- app/src/main/java/com/chaoyi/myapplication/MusicPlay.java
+++ app/src/main/java/com/chaoyi/myapplication/MusicPlay.java
@@ -7,14 +7,7 @@ import android.media.AudioTrack;
 
 public class MusicPlay {
     static {
-        System.loadLibrary("avcodec");
-        System.loadLibrary("avfilter");
-        System.loadLibrary("avformat");
-        System.loadLibrary("avutil");
-        System.loadLibrary("swresample");
-        System.loadLibrary("swscale");
         System.loadLibrary("ffmpegdemo");
-
     }
 
     public native void playSound(String input);
